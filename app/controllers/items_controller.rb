85a7bb00dad88287bcd78ab62a743f2443923c81
Alberto <alberto.grau.margarida@gmail.com>
2016-04-01 18:38:37 -0400

    Created To-Do Items CP completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,31 @@
 class ItemsController < ApplicationController
-  def new
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to current_user, notice: "Your item was saved"
+    else
+      flash[:error] = "Error creating item. Please try again."
+      render "users/show"
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "\"#{@item.name}\" was deleted succesfully."
+    else
+      flash[:error] = "There was an error deleting the task."      
+    end
+    redirect_to current_user
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  get 'items/new'
-
-
   devise_for :users
   resources :users, only: [:show] do
-    resources :items
+    resources :items, only: [:create, :destroy]
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-root to: "welcome#index"
+root to: 'users#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
